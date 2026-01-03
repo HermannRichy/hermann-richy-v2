368d26c8108e923020929e9d11b4a89ab51d32da
Hermann Richy <hermannrichy15@gmail.com>
2026-01-03 15:09:03 +0100

    start hero section

---
+++ src/components/sections/Header.tsx
@@ -0,0 +1,59 @@
+import BubbleMenu from "@/components/BubbleMenu";
+
+export default function Header() {
+    const items = [
+        {
+            label: "Accueil",
+            href: "#hero",
+            ariaLabel: "Home",
+            rotation: -8,
+            hoverStyles: { bgColor: "#3b82f6", textColor: "#ffffff" },
+        },
+        {
+            label: "A propos",
+            href: "#about",
+            ariaLabel: "About",
+            rotation: 8,
+            hoverStyles: { bgColor: "#10b981", textColor: "#ffffff" },
+        },
+        {
+            label: "Skills",
+            href: "#skills",
+            ariaLabel: "Skills",
+            rotation: 8,
+            hoverStyles: { bgColor: "#ef4444", textColor: "#ffffff" },
+        },
+        {
+            label: "Projets",
+            href: "#projects",
+            ariaLabel: "Projects",
+            rotation: 8,
+            hoverStyles: { bgColor: "#f59e0b", textColor: "#ffffff" },
+        },
+        {
+            label: "contact",
+            href: "#",
+            ariaLabel: "Contact",
+            rotation: -8,
+            hoverStyles: { bgColor: "#8b5cf6", textColor: "#ffffff" },
+        },
+    ];
+
+    return (
+        <BubbleMenu
+            logo={
+                <span className="text-lg text-background font-bold font-ciguatera">
+                    Hermann Richy
+                </span>
+            }
+            items={items}
+            menuAriaLabel="Toggle navigation"
+            menuBg="#ffffff"
+            menuContentColor="#111111"
+            useFixedPosition={true}
+            animationEase="back.out(1.5)"
+            animationDuration={0.5}
+            staggerDelay={0.12}
+        />
+    );
+}
